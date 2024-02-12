32f3cff7102374e5eff7b3e7e153519214d5bc87
OgataiKhan <badellino.kevin@gmail.com>
2024-02-12 07:35:13 +0100

    fix: handle null case

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -4,7 +4,7 @@
     <div class="container pt-4">
         <h1>{{ $project->title }}</h1>
         <p>{{ $project->description }}</p>
-        <p>Type: {{ $project->type->title }}</p>
+        <p>Type: {{ $project->type->title ?? 'None' }}</p>
         <a href="{{ route('admin.projects.index') }}" role="button" class="btn btn-primary">Projects list</a>
     </div>
 @endsection
